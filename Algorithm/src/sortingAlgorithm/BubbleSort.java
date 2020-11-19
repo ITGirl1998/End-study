e3940c52b7220f265b049e03acbf0920c96ae1de
lishiying6 <lishiying6@jd.com>
2020-11-19 12:09:00 +0800

    常见的排序算法

--- Algorithm/src/sortingAlgorithm/BubbleSort.java
+++ Algorithm/src/sortingAlgorithm/BubbleSort.java
@@ -3,30 +3,28 @@ package sortingAlgorithm;
 public class BubbleSort {
     public static void main(String[] args) {
         int nums[] = new int[10000];
-        for (int i = 1000; i >0 ; i--) {
-            nums[1000-i]=i;
+        for (int i = 1000; i > 0; i--) {
+            nums[1000 - i] = i;
         }
-        System.out.println( "Arrays.toString(BubbleSort(nums))");
+        System.out.println("Arrays.toString(BubbleSort(nums))");
     }
 
     public static int[] BubbleSort(int[] nums) {
-        long startTime=System.currentTimeMillis();
-        for (int i = 0; i <nums.length-1 ; i++) {
-            int swap = 0;
-            for (int j = i+1; j < nums.length; j++) {
-                if(nums[i]>nums[j]){
-                    swap++;
+        for (int i = 0; i < nums.length - 1; i++) {
+            boolean swap = false;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] > nums[j]) {
                     int num = nums[i];
                     nums[i] = nums[j];
                     nums[j] = num;
+                    swap = true;
                 }
             }
-            if(swap==0){
+            //优化点： // 如果某次未发生数据交换，说明数据已排序
+            if (!swap) {
                 break;
             }
         }
-        long endTime=System.currentTimeMillis();
-        System.out.println("运行时间"+(endTime-startTime));
         return nums;
     }
 }
